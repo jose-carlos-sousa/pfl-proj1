0ca58e4b71c782aef893e2b7bef5d9b2d03efbe7
Jose Sousa <2409jmsousa@gmail.com>
2024-11-02 14:47:21 +0000

    change code structure

--- TP1.hs
+++ TP1.hs
@@ -2,6 +2,16 @@ import qualified Data.List
 import qualified Data.Array
 import qualified Data.Bits
 
+type City = String
+type Path = [City]
+type Distance = Int
+type RoadMap = [(City,City,Distance)]
+type AdjList = [(City,[(City,Distance)])]
+type Matrix = Data.Array.Array (Int,Int) (Maybe Distance)
+type TspCoord = (Int, Set)
+type TspEntry = (Int, [Int]) -- An entry in the table is a tuple consisting of the value c (of type Int) and the corresponding shortest path (a list of vertices).
+
+
 newTable ::(Data.Array.Ix b) =>[(b, a)] -> Table a b
 findTable ::(Data.Array.Ix b) =>Table a b -> b -> a
 updTable ::(Data.Array.Ix b) => (b,a) -> Table a b -> Table a b
@@ -24,12 +34,6 @@ dynamic :: Data.Array.Ix coord => (Table entry  coord -> coord -> entry) -> (coo
 dynamic compute bnds = t
     where t = newTable (map ( \coord -> ( coord , compute t coord) ) (Data.Array.range bnds ) )
 
-type City = String
-type Path = [City]
-type Distance = Int
-type RoadMap = [(City,City,Distance)]
-type AdjList = [(City,[(City,Distance)])]
-type Matrix = Data.Array.Array (Int,Int) (Maybe Distance)
 
 -- Helper function to replace fromMaybe
 fromMaybe :: a -> Maybe a -> a
@@ -78,6 +82,57 @@ roadMapToMatrix roadMap = mkGraph False bounds edges
     edges = [((cityIndex roadMap c1) + 1, (cityIndex roadMap c2) + 1, d) | (c1, c2, d) <- roadMap]
 
    
+mkGraph :: Bool -> (Int, Int) -> [(Int, Int, Distance)] -> Matrix
+mkGraph dir bnds@(1,u) es
+    = emptyArray Data.Array.// ([((x1,x2), Just w) | (x1,x2,w) <-es]++ if dir then [] else [((x2,x1), Just w) | (x1 ,x2 ,w) <-es , x1 /=x2])
+    where emptyArray = Data.Array.array ((1,1),(u,u)) [((x1,x2), Nothing) | x1 <- Data.Array.range bnds, x2 <- Data.Array.range bnds]
+
+
+nodes :: Matrix -> [Int]
+nodes g = Data.Array.range (1,u ) where ((1, _), (u,_)) = Data.Array.bounds g
+
+weight :: Int -> Int -> Matrix -> Distance
+weight x y g = let w = g Data.Array.! (x, y) in
+    case w of
+        Just weightValue -> weightValue
+        Nothing -> maxBound
+
+adjacentNodes :: Matrix -> Int -> [Int]
+adjacentNodes g v1 = [v2 | v2 <- nodes g, (g Data.Array.! (v1, v2)) /= Nothing]
+
+type Set = Integer  
+
+emptySet :: Set
+emptySet = 0
+
+setEmpty :: Set -> Bool
+setEmpty n = n == 0
+
+
+fullSet :: Int -> Set
+fullSet n = 2 ^ (n + 1) - 2
+
+-- Adds a bit to a mask
+addSet :: Int -> Set -> Set
+addSet i s = d_ * e + m
+    where (d, m) = divMod s e
+          e = 2 ^ i
+          d_ = if odd d then d else d + 1
+
+-- Removes a bit from a mask
+delSet :: Int -> Set -> Set
+delSet i s = d_ * e + m
+    where (d, m) = divMod s e
+          e = 2 ^ i
+          d_ = if odd d then d - 1 else d
+
+-- Converts a set to a list
+set2List :: Set -> [Int]
+set2List s = s2l s 0
+    where s2l 0 _ = []
+          s2l n i | odd n   = i : s2l (n `div` 2) (i + 1)
+                   | otherwise = s2l (n `div` 2) (i + 1)
+
 
 -- Given a roadmap returns unique cities
 -- Does this by using nub which returns unique elements in a List
@@ -216,60 +271,6 @@ fib :: Int -> Int
 fib n = findTable t n
   where t = dynamic compFib (bndsFib n)
 
-
-mkGraph dir bnds@(1,u) es
-    = emptyArray Data.Array.// ([((x1,x2), Just w) | (x1,x2,w) <-es]++ if dir then [] else [((x2,x1), Just w) | (x1 ,x2 ,w) <-es , x1 /=x2])
-    where emptyArray = Data.Array.array ((1,1),(u,u)) [((x1,x2), Nothing) | x1 <- Data.Array.range bnds, x2 <- Data.Array.range bnds]
-
-
-
-nodes g = Data.Array.range (1,u ) where ((1, _), (u,_)) = Data.Array.bounds g
-
-weight x y g = let w = g Data.Array.! (x, y) in
-    case w of
-        Just weightValue -> weightValue
-        Nothing -> maxBound
-
-
-type Set = Integer  
-
-emptySet :: Set
-emptySet = 0
-
-setEmpty :: Set -> Bool
-setEmpty n = n == 0
-
-
-fullSet :: Int -> Set
-fullSet n = 2 ^ (n + 1) - 2
-
--- Adds a bit to a mask
-addSet :: Int -> Set -> Set
-addSet i s = d_ * e + m
-    where (d, m) = divMod s e
-          e = 2 ^ i
-          d_ = if odd d then d else d + 1
-
--- Removes a bit from a mask
-delSet :: Int -> Set -> Set
-delSet i s = d_ * e + m
-    where (d, m) = divMod s e
-          e = 2 ^ i
-          d_ = if odd d then d - 1 else d
-
--- Converts a set to a list
-set2List :: Set -> [Int]
-set2List s = s2l s 0
-    where s2l 0 _ = []
-          s2l n i | odd n   = i : s2l (n `div` 2) (i + 1)
-                   | otherwise = s2l (n `div` 2) (i + 1)
-
-type TspCoord = (Int, Set)
-type TspEntry = (Int, [Int]) -- An entry in the table is a tuple consisting of the value c (of type Int) and the corresponding shortest path (a list of vertices).
-
-adjacentNodes :: Matrix -> Int -> [Int]
-adjacentNodes g v1 = [v2 | v2 <- nodes g, (g Data.Array.! (v1, v2)) /= Nothing]
-
 compTsp :: Matrix -> Int -> Table TspEntry TspCoord -> TspCoord -> TspEntry
 compTsp g n a (i, k)
   | setEmpty k = 
@@ -304,7 +305,7 @@ tsp g
 travelSales :: RoadMap -> Path
 travelSales roadmap = map (cities roadmap !!) (map (\n -> n - 1) (snd (tsp (roadMapToMatrix roadmap))))
 
-
+-- Some graphs to test your work
 gTest1 :: RoadMap
 gTest1 = [("7","6",1),("8","2",2),("6","5",2),("0","1",4),("2","5",4),("8","6",6),("2","3",7),("7","8",7),("0","7",8),("1","2",8),("3","4",9),("5","4",10),("1","7",11),("3","5",14)]
 
@@ -312,33 +313,4 @@ gTest2 :: RoadMap
 gTest2 = [("0","1",10),("0","2",15),("0","3",20),("1","2",35),("1","3",25),("2","3",30)]
 
 gTest3 :: RoadMap -- unconnected graph
-gTest3 = [("0", "1", 5), ("1", "2", 10), ("2", "0", 15)]
-
-gTest4 :: RoadMap -- unconnected graph
-gTest4 = [("0", "1", 5), ("0", "2", 21), ("0", "3", 38), ("0", "4", 46), ("0", "5", 40),
-           ("0", "6", 27), ("0", "7", 114), ("0", "8", 43), ("0", "9", 26), ("0", "10", 34),
-           ("1", "2", 25), ("1", "3", 41), ("1", "4", 48), ("1", "5", 43), ("1", "6", 29),
-           ("1", "7", 118), ("1", "8", 44), ("1", "9", 24), ("1", "10", 38),
-           ("2", "3", 39), ("2", "4", 33), ("2", "5", 21), ("2", "6", 18), ("2", "7", 94),
-           ("2", "8", 37), ("2", "9", 31), ("2", "10", 15),
-           ("3", "4", 72), ("3", "5", 57), ("3", "6", 57), ("3", "7", 112), ("3", "8", 75),
-           ("3", "9", 63), ("3", "10", 51),
-           ("4", "5", 17), ("4", "6", 20), ("4", "7", 83), ("4", "8", 16), ("4", "9", 34),
-           ("4", "10", 22),
-           ("5", "6", 19), ("5", "7", 78), ("5", "8", 29), ("5", "9", 38), ("5", "10", 6),
-           ("6", "7", 96), ("6", "8", 19), ("6", "9", 19), ("6", "10", 18),
-           ("7", "8", 99), ("7", "9", 115), ("7", "10", 81),
-           ("8", "9", 23), ("8", "10", 32),
-           ("9", "10", 36)]
-main :: IO ()
-main = do
-    let citi = cities gTest1
-    let bounds = (1, length citi)
-    let edges = [(cityIndex gTest1 c1 + 1, cityIndex gTest1 c2 + 1, d) | (c1, c2, d) <- gTest1]
-    print $ "Cities: " ++ show citi
-    print $ "Bounds: " ++ show bounds
-    print $ "Edges: " ++ show edges
-    let graph = mkGraph False bounds edges
-    print $ "Graph: " ++ show graph
-    let (cost, path) = tsp graph  -- Call the TSP function
-    putStrLn $ "Minimum cost: " ++ show cost ++ ", Path: " ++ show path
+gTest3 = [("0","1",4),("2","3",2)]
